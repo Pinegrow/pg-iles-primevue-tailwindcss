3f7126ad0d1649c16e367dac57d12fb5cb4461d3
Akayy <techakayy@gmail.com>
2024-11-15 14:22:47 +1100

    Initial commit

---
+++ src/components/NavBarHamburger.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import { useNavMenu } from '@/composables/nav-menu'
+  const { isNavMenuOpen, openNavMenu } = useNavMenu()
+</script>
+<template>
+  <BaseButton
+    size="xl"
+    :icon="
+      isNavMenuOpen ? 'i-material-symbols-close' : 'i-material-symbols-menu'
+    "
+    class="!py-2 !rounded-lg leading-none"
+    @click="openNavMenu"
+    ><span class="sr-only">Open main menu</span>
+  </BaseButton>
+</template>
+<style scoped></style>
