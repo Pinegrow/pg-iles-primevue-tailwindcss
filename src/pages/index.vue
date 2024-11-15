223088d3f34c57b1f9ea099659bad2309b0377c9
Akayy <techakayy@gmail.com>
2024-11-15 14:51:35 +1100

    Minimal template

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,22 @@
 <page>
-    title: Home
+  title: Home
 </page>
 <template>
   <div>
-    <HeroSection />
-    <AboutTemplate />
-    <WalkThrough />
+    <div
+      style="
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        padding-top: 1.5rem /* 24px */;
+        padding-bottom: 1.5rem /* 24px */;
+      "
+    >
+      <DarkModeSwitch client:load /><span style="margin-left: 0.5rem; /* 8px */"
+        >A Fresh Start!</span
+      >
+    </div>
   </div>
 </template>
 <style scoped></style>
